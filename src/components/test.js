d15f41d04ed3d7fb64fbf7ec8331280d635c6cbe
Bartek Siatkowski <bartek.siatkowski@interia.pl>
2021-11-18 13:15:13 +0100

    test

---
+++ src/components/test.js
@@ -0,0 +1,15 @@
+//kopia karta.js
+import { Link } from 'react-router-dom'
+import Stopka from "./Stopka"
+
+function Karta() {
+    return (
+      <div id="karta">
+        Karta
+        <Stopka />
+      </div>
+    );
+  }
+  
+  export default Karta;
+  
